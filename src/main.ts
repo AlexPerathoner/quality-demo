82ab6859305f2a49bfa89a34d0ff6c4e836093de
Alexander Perathoner <alexander.perathoner@targomo.com>
2021-09-07 15:49:57 +0200

    Updating config

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
+console.log(environment.Targomo_API_KEY)
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
