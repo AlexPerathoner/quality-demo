6ef4f341d54dbf70132fe9d888437e61333b23a2
Alexander Perathoner <alexander.perathoner@targomo.com>
2021-09-22 15:00:10 +0200

    Separating material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DynamicComponentService } from 'services/dynamic-component.service';
+import { PoiTreeModule } from '../lib/poi-tree/poi-tree.module'
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { WidgetComponent } from './widget/widget.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BtnComponent } from './btn/btn.component';
-import { MatIconModule } from '@angular/material/icon';
 import { SettingsViewComponent } from './settings-view/settings-view.component';
 import { LocationWidgetComponent } from './location-widget/location-widget.component';
-import { DynamicComponentService } from 'services/dynamic-component.service';
 import { PopupComponent } from './popup/popup.component';
 import { SettingsBtnComponent } from './settings-btn/settings-btn.component';
 import { ResetBtnComponent } from './reset-btn/reset-btn.component';
-import { MatOptionModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
 import { DetailsWidgetComponent } from './details-widget/details-widget.component';
 import { ProgressCircleComponent } from './progress-circle/progress-circle.component';
 import { OptionBtnComponent } from './option-btn/option-btn.component';
-import { MatSliderModule } from '@angular/material/slider';
 import { ModalComponent } from './modal/modal.component';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { PoiTreeModule } from '../lib/poi-tree/poi-tree.module'
 
 
 @NgModule({
@@ -49,16 +41,8 @@ import { PoiTreeModule } from '../lib/poi-tree/poi-tree.module'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    MatOptionModule,
-    MatSelectModule,
-    MatSliderModule,
-    MatExpansionModule,
-    MatTooltipModule,
-    MatCheckboxModule,
     PoiTreeModule,
+    MaterialModule
   ],
   providers: [DynamicComponentService],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatSlideToggleModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatExpansionModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+  ]
+})
+export class MaterialModule { }
